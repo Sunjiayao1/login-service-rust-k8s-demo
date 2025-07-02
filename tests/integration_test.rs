b75bbabdea7b0d6c0a749f9ec2de71ad2215c9c0
jiayao-sun <jiayao.sun@rea-group.com>
2025-07-02 09:12:25 +0800

    modify tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,30 +1,46 @@
 #[cfg(test)]
 mod tests {
-    use reqwest::Client;
     use tokio;
     use std::error::Error;
-    use serde_json::Value;
+    use warp::Filter;
+    use warp::test::request;
+
+    fn api_filter() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+        warp::path("healthz").map(|| "Healthy!")
+            .or(warp::path("auth")
+                .and(warp::header::exact("authorization", "Basic dXNlcm5hbWU6cGFzc3dvcmQ=")) // base64 username:password
+                .map(|| "Hello, protected endpoint! Authenticate!"))
+    }
 
     #[tokio::test]
     async fn test_public_endpoint() -> Result<(), Box<dyn Error>> {
-        let response = reqwest::get("http://localhost:8081/healthz").await?;
-        assert_eq!(response.status(), 200);
-        let body = response.text().await?;
-        assert_eq!(body, "Healthy!");
+        let filter = api_filter();
+        let resp = request()
+            .method("GET")
+            .path("/healthz")
+            .reply(&filter)
+            .await;
+
+        assert_eq!(resp.status(), 200);
+        assert_eq!(resp.body(), "Healthy!");
         Ok(())
+
     }
 
     #[tokio::test]
     async fn test_protected_endpoint_with_basic_auth() -> Result<(), Box<dyn Error>> {
-        let client = Client::new();
-        let response = client
-            .get("http://localhost:8081/auth")
-            .basic_auth("username", Some("password"))
-            .send()
-            .await?;
-        assert_eq!(response.status(), 200);
-        let body = response.text().await?;
-        assert_eq!(body, "Hello, protected endpoint! Authenticate!");
+        let filter = api_filter();
+        let auth_header_value = "Basic dXNlcm5hbWU6cGFzc3dvcmQ=";
+
+        let resp = request()
+            .method("GET")
+            .path("/auth")
+            .header("authorization", auth_header_value)
+            .reply(&filter)
+            .await;
+
+        assert_eq!(resp.status(), 200);
+        assert_eq!(resp.body(), "Hello, protected endpoint! Authenticate!");
         Ok(())
     }
 }
